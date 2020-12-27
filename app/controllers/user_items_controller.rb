b3a20c4fa236e9f4e566f02573a6846a09403359
samirajil <samirajeilinger@gmail.com>
2020-12-27 10:13:05 +0100

    controllers

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,15 +1,27 @@
 class UserItemsController < ApplicationController
   def new
     @useritem = UserItem.new
+    @items = Item.all
   end
 
   def create
-    @useritem = UserItem.find(params[:id])
+    @useritem = UserItem.new()
+    @useritem.number = params[:user_item][:number].to_i
+    @item = Item.find(params[:user_item][:item].to_i)
+
+    @useritem.item = @item
+    @useritem.user = current_user
     
     if @useritem.save
+      raise
+      current_user.points += @useritem[:number] * @item[:poins]
       redirect_to players_index_path
     else
       render new
     end
+  end
+
+  def useritem_params
+    params.require(:user_item).permit(:item, :number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
   root to: 'pages#home'
+
+  resources :user_items, only: [:index, :new, :create]
+  resources :players, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
