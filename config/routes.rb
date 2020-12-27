8814ccb7487913e661697c51c316a9ba555a9668
samirajil <samirajeilinger@gmail.com>
2020-12-27 14:42:14 +0100

    photo

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+
+
   resources :user_items, only: [:index, :new, :create]
   resources :players, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
